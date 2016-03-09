a0485225b9b21c0dbb57b3e09d80882020c5893d
freeman <the.freeman.lab@gmail.com>
2016-03-09 18:07:35 -0500

    add test for multiple replacement

--- test.js
+++ test.js
@@ -29,7 +29,23 @@ test('replacement', function (t) {
   }
   var result = material(gl, flat, {'COUNT': 10})
   t.ok(result.shader, 'shader defined')
-  t.equal(result.shader._fref.src.indexOf('test[10]'), 36)
+  t.equal(result.shader._fref.src.indexOf('test[10]'), 36, 'line found')
+  t.deepEqual(result.defaults.color, [0, 100, 0])
+  t.end()
+})
+
+test('multiple replacement', function (t) {
+  var flat = {
+    fragment: 'precision highp float; uniform vec3 a[COUNT]; uniform vec3 b[COUNT]; struct Style {vec3 color;};uniform Style style;void main() {gl_FragColor = vec4(style.color, 1.0);}',
+    style: {
+      color: {type: 'vec3', default: [0, 100, 0]}
+    },
+    name: 'my-flat-material'
+  }
+  var result = material(gl, flat, {'COUNT': 10})
+  t.ok(result.shader, 'shader defined')
+  t.equal(result.shader._fref.src.indexOf('a[10]'), 36, 'line found')
+  t.equal(result.shader._fref.src.indexOf('b[10]'), 56, 'line found')
   t.deepEqual(result.defaults.color, [0, 100, 0])
   t.end()
 })
